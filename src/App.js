daafae2a3e2cadd4962506444091be66a7bf3c5b
Claholla <claholla@gmail.com>
2021-09-25 20:59:15 -0500

    bug fixes

--- src/App.js
+++ src/App.js
@@ -17,6 +17,9 @@ function App() {
           <Route exact path="/">
             <Homepage />
           </Route>
+          <Route exact path="/react-portfolio/">
+            <Homepage />
+          </Route>
           <Route exact path="/about">
             <Homepage />
           </Route>
